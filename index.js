6e9f3c8224488e2bd674e46c0bf7c6256f220d73
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-15 13:31:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value
   return driver
 }
+
+function deleteFromDriverByKey(driver, key) {
+  let newObj = Object.assign({}, driver)
+  delete newObj[key]
+  return newObj
+}
